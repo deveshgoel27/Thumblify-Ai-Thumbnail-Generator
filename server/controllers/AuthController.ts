97477770c288f534529a342238a0fbf7562108b9
Devesh goel <techdevesh2702@gmail.com>
2026-03-14 15:15:12 +0530

    Completed Thumbnail generator  API

--- server/controllers/AuthController.ts
+++ server/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User from "../configs/models/User";
+import User from "../models/User";
 import bcrypt from "bcrypt";
  
 //   Controllers for user registeration 
